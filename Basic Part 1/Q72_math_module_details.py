da894ec91cb4ee84dd00d073e23eed57c30c79a4
sdhaval97 <sdhaval97@gmail.com>
2023-03-27 22:54:37 -0400

    solved question 72

---
+++ Basic Part 1/Q72_math_module_details.py
@@ -0,0 +1,11 @@
+# Q. To get the details of the math module
+
+# importing the math module
+import math
+
+# listing all the functions present in math module
+math_list = dir(math)
+print(math_list)
+
+# the dir function is used to retrieve all the functions and variables
+# available in the math module
